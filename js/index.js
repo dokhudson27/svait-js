dfd3f1417390aca952933bc6be088e3270b8b596
Sviatoslav Kovalov <dokhudson27@gmail.com>
2020-07-11 17:20:12 +0300

    3

---
+++ js/index.js
@@ -0,0 +1,33 @@
+//let greet = 'Hello world';
+//alert (greet);
+//greet='Goodbye'
+//alert(greet);
+
+//String
+//const someName = 'Jon';
+
+//number
+//const age = 42.5;
+
+//Bollean
+//const married = true; //false
+
+//underfind
+//const notDefined=undefined;
+//let notDefined
+
+//null
+//cosnt empty=null;
+
+//Object
+//const someObj={};
+
+//Sumbol
+//const someSym=Symbol();
+
+const user=prompt('enter your name');
+document.write('hello'+user);
+document.write('<br>');
+const age = prompt('Enter your age');
+const nextAge = age + 1;
+document.write('next year you will be'+ nextage);
